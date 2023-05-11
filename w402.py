d5c6bc054abb04247d130d268b23dcfad5e58d5c
vetsler <vetsler@gmail.com>
2023-05-11 23:21:54 +0200

    moved comments

--- w402.py
+++ w402.py
@@ -4,24 +4,11 @@
 # Давайте реализуем похожий механизм с помощью дескрипторов. Напишите дескриптор Value, 
 # который будет использоваться в нашем классе Account.
 
-class Value:
-    #я думаю, что надо сделать проверку на то, 
-    # что amount не может быть отрицательным 
-    # и не может быть текстом 
-    # commision can not be taken from negative value
 
-    def __init__(self):
-        self.value = None
-    
-    @staticmethod
-    def _calculate_value(value, comm):
-        return value * (1 - comm) 
-
-    def __get__(self, obj, obj_type): 
-        return self.value
-    
-    def __set__(self, obj, value): 
-        self.value = self._calculate_value(value, obj.commission)
+#я думаю, что надо сделать проверку на то, 
+# что amount не может быть отрицательным 
+# и не может быть текстом 
+# commision can not be taken from negative value
 
 class Account:
     amount = Value() #amount это атрибут класса
@@ -31,6 +18,14 @@ class Account:
 
 # У аккаунта будет атрибут commission. 
 # Именно эту коммиссию и нужно вычитать при присваивании значений в amount.
+
+"""
+new_account = Account(0.1)
+new_account.amount = 100
+
+print(new_account.amount)
+90"""
+
 if __name__ == "__main__":
 
     new_account = Account(0.02)
