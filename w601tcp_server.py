1eea365f13793e400068abbde8b3a711ee18c0aa
vetsler <vetsler@gmail.com>
2023-05-26 15:42:05 +0200

    made tcp server responsive to put request

--- w601tcp_server.py
+++ w601tcp_server.py
@@ -9,12 +9,18 @@ async def handle_echo(reader, writer):
     addr = writer.get_extra_info("peername")
     print("received %r from %r" % (message, addr))
     
-    writer.close()
+    response = 'ok\n\n'
+    
+    print(f"Send: {response!r}")
+    writer.write(response.encode())
+    await writer.drain()
 
+    
 loop = asyncio.get_event_loop()
 coro = asyncio.start_server(handle_echo, "127.0.0.1", 10001,
                             loop=loop)
 server = loop.run_until_complete(coro)
+print('running until complete')
 
 #будем обрабатывать все входящие соединения, 
 # и после того, как мы заакцептили соединение, 
@@ -23,9 +29,11 @@ server = loop.run_until_complete(coro)
 
 try:
     loop.run_forever()
+    print('running forever')
 except KeyboardInterrupt:
-    pass
-
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+    print('closing everything:\n')
+    server.close()
+    print('server closed\n')
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+    print('closing loop')
