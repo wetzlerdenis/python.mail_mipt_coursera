ed38f3073cd8b76e9d604e03abb4af430bcd191a
vetsler <vetsler@gmail.com>
2023-05-26 16:42:59 +0200

    added async run server func, process data func

--- w601solution.py
+++ w601solution.py
@@ -1,28 +1,78 @@
-import asyncio
+# week6
+# Task 1 Server for receiving metrics
 
+"""
+Необходимо реализовать серверную часть.
+
+Требоания 
+1) По запросу put требуется сохранять метрики в структурах данных в памяти процесса. 
+2) По запросу get сервер обязан отдавать данные в правильной последовательности. 
+3) При работе с клиентом сервер должен поддерживать сессии, соединение с 
+клиентом между запросами не должно "разрываться".
+4) На верхнем уровне модуля должна быть объявлена функция run_server(host, port) 
+5) Все запросы будут выполняться с таймаутом
+6) должен уметь обрабатывать запросы от нескольких клиентов одновременно.
+7) должен отдавать клиенту ошибку в формате, оговоренном в протоколе. 
+В этих случаях работа сервера не должна завершаться аварийно.
+
+Пример tcp-сервера на asyncio:
+1) Данный код создает tcp-соединение для адреса 127.0.0.1:8181 
+2) слушает все входящие запросы. 
+3) При подключении клиента будет создан новый экземпляр класса ClientServerProtocol, 
+4) при поступлении новых данных вызовется метод объекта - data_received
+
+Внутри asyncio.Protocol спрятана вся магия обработки запросов через корутины, 
+остается реализовать протокол взаимодействия между клиентом и сервером.
+"""
+
+import asyncio
 
 class ClientServerProtocol(asyncio.Protocol):
     def connection_made(self, transport):
+        peername = transport.get_extra_info('peername')
+        print('Connection from {}'.format(peername))
         self.transport = transport
 
     def data_received(self, data):
-        resp = process_data(data.decode())
+        messsage = data.decode()
+        print('Received: ',messsage)
+
+        resp = process_data(messsage)
+
+        print('Send: ', resp)
         self.transport.write(resp.encode())
 
+    def connection_lost(self, exc):
+        return super().connection_lost(exc)
+    
+async def run_server(host,port):
+    loop = asyncio.get_event_loop()
+    
+    coro = await loop.create_server(
+        ClientServerProtocol,
+        host,
+        port
+        )
+    
+    server = loop.run_until_complete(coro)
 
-loop = asyncio.get_event_loop()
-coro = loop.create_server(
-    ClientServerProtocol,
-    '127.0.0.1', 8181
-)
+    try:
+        loop.run_forever()
+        print('running forever')
+    except KeyboardInterrupt:
+        print('closing everything:\n')
+        server.close()
+        loop.run_until_complete(server.wait_closed())
+        loop.close()
 
-server = loop.run_until_complete(coro)
+def process_data(data):
+    response = {}
+    request, payload = data.split(' ',1)
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+    if request == 'get':
+        response = 'ok\n cpu 2\n'
+    elif request == 'put':
+        response = 'ok\n\n'
+    return response
 
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+asyncio.run(run_server)
